9292ce24dfc579e5e68ebc698ced3b19c041a072
tacoen <tacoen@gmail.com>
2013-08-16 15:07:46 +0900

    Update pico_editor_config.php
    if basename() doesn't work, we had to put variable and populate   pico_editor.php -- sorry bad english

--- pico_editor_config.php
+++ pico_editor_config.php
@@ -1,11 +1,19 @@
 <?php
 
 global $pico_editor_password;
+global $pico_root_path;
 
-/* 
+/*
  * This should be a sha1 hash of your password.
  * Use a tool like http://www.sha1-online.com to generate.
  */
+
 $pico_editor_password = '';
+
+/*
+* if basename() doesn't work
+*/
+
+$pico_root_path = "pico";
 
 ?>
